6ed86cd610622c074bbf490f1e71bc7f9ec3900b
= <alvin@konvergentgroup.com>
2023-08-23 14:00:28 +0800

    add example msgs

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -13,7 +13,9 @@ bot.start(ctx => {
 })
 
 bot.help((ctx) => ctx.reply('Send me a sticker'));
-
+bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.launch();
 // AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
 // essential syntax that is forgotten by all tutorials
 

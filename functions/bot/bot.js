7d94fa0a1f0d754f06a0aaed7e295482bc5727b8
= <alvin@konvergentgroup.com>
2023-08-23 14:35:22 +0800

    login-bot

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -4,35 +4,19 @@ const { message } = require('telegraf/filters');
 // BOT_TOKEN is env var in netlify
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.start(ctx => {
-  console.log("Received /start command")
-  try {
-    return ctx.reply("Hi alvin")
-  } catch (e) {
-    console.error("error in start action:", e)
-    return ctx.reply("Error occured")
-  }
-})
-
 const keyboard = Markup.inlineKeyboard([
+	Markup.button.login("Login", "http://domain.tld/hash", {
+		bot_username: "my_bot",
+		request_write_access: true,
+	}),
 	Markup.button.url("❤️", "http://telegraf.js.org"),
 	Markup.button.callback("Delete", "delete"),
 ]);
 
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-bot.command('oldschool', (ctx) => ctx.reply('Hello'));
-bot.command('hipster', Telegraf.reply('λ'));
-// bot.on("message", ctx => ctx.copyMessage(ctx.message.chat.id, keyboard));
-// bot.action("delete", ctx => ctx.deleteMessage());
-bot.on(message('text'), async (ctx) => {
-    // Explicit usage
-    await ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`);
-  
-    // Using context shortcut
-    await ctx.reply(`Hello ${ctx.state.role}`);
-  });
+bot.start(ctx => ctx.reply("Hello", keyboard));
+bot.action("delete", ctx => ctx.deleteMessage());
+
+
 
 // AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
 // essential syntax that is forgotten by all tutorials

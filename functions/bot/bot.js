71f632e085738802938193d73c7118a983346298
= <alvin@konvergentgroup.com>
2023-08-23 14:48:19 +0800

    poll-bot

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -4,14 +4,25 @@ const { message } = require('telegraf/filters');
 // BOT_TOKEN is env var in netlify
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-const keyboard = Markup.inlineKeyboard([
-	Markup.button.login("Login", "https://www.google.com/", {
-		bot_username: "partyeebot",
-		request_write_access: true,
-	}),
-	Markup.button.url("❤️", "http://telegraf.js.org"),
-	Markup.button.callback("Delete", "delete"),
+const keyboard = Markup.keyboard([
+	Markup.button.pollRequest("Create poll", "regular"),
+	Markup.button.pollRequest("Create quiz", "quiz"),
 ]);
+
+
+bot.on("poll", ctx => console.log("Poll update", ctx.poll));
+bot.on("poll_answer", ctx => console.log("Poll answer", ctx.pollAnswer));
+
+bot.start(ctx => ctx.reply("supported commands: /poll /quiz", keyboard));
+
+bot.command("poll", ctx =>
+	ctx.replyWithPoll("Your favorite math constant", ["x", "e", "π", "φ", "γ"], {
+		is_anonymous: false,
+	}),
+);
+bot.command("quiz", ctx =>
+	ctx.replyWithQuiz("2b|!2b", ["True", "False"], { correct_option_id: 0 }),
+);
 
 bot.start(ctx => ctx.reply("Hello", keyboard));
 bot.action("delete", ctx => ctx.deleteMessage());
